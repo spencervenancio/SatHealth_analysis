f41f40f3c1aea91bdb444b4e0ca6c147c20824dc
spencervenancio <spencervenancio@gmail.com>
2026-03-18 17:14:33 -0500

    added index parameter to create_target

--- src/data/targets.py
+++ src/data/targets.py
@@ -3,11 +3,12 @@ import pandas as pd
 def create_target(
     icd_df: pd.DataFrame, 
     code: str = 'F32', # ICD code for depression 
-    code_str: str = 'depr'
+    code_str: str = 'depr', 
+    index: str | list[str] = ['COUNTYFP', 'year']
     ) -> pd.DataFrame:
     target_col = f"{code_str}_prev"
     df = icd_df[icd_df['code'] == code][[
         'year', 'prevalence'
     ]]
-    df = df.rename(columns={'prevalence': target_col})
-    return df
+    df = df.rename(columns={'prevalence': target_col}).reset_index()
+    return df.set_index(index)
